86c56c2acb5cf9bc00ccb19685112b1c74c5f28d
Valérian Rey <valerian.rey@gmail.com>
2025-05-01 19:01:21 +0200

    Add configurations.py

---
+++ src/aggregator_arena/configurations.py
@@ -0,0 +1,20 @@
+import torch
+
+from aggregator_arena.runtime import Runtime
+
+OBJECTIVE_LISTS = {
+    "runtime_0": [
+        Runtime(m=1, n=1000, device="cuda", dtype=torch.float32, iterations=10),
+        Runtime(m=10, n=1000, device="cuda", dtype=torch.float32, iterations=10),
+        Runtime(m=20, n=1000, device="cuda", dtype=torch.float32, iterations=10),
+        Runtime(m=30, n=1000, device="cuda", dtype=torch.float32, iterations=10),
+        Runtime(m=40, n=1000, device="cuda", dtype=torch.float32, iterations=10),
+        Runtime(m=50, n=1000, device="cuda", dtype=torch.float32, iterations=10),
+        Runtime(m=60, n=1000, device="cuda", dtype=torch.float32, iterations=10),
+        Runtime(m=70, n=1000, device="cuda", dtype=torch.float32, iterations=10),
+        Runtime(m=80, n=1000, device="cuda", dtype=torch.float32, iterations=10),
+        Runtime(m=90, n=1000, device="cuda", dtype=torch.float32, iterations=10),
+        Runtime(m=100, n=1000, device="cuda", dtype=torch.float32, iterations=10),
+        Runtime(m=120, n=1000, device="cuda", dtype=torch.float32, iterations=10),
+    ],
+}
