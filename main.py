26a0f858f3a2da10900eeb615b17d179434b5b5d
Valérian Rey <valerian.rey@gmail.com>
2025-05-01 22:22:59 +0200

    Fix main

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ import pandas as pd
 from pprint import pprint
 
 from arena.configurations import OBJECTIVE_LISTS
-from torchjd.aggregation import *  # noqa
-
+from arena.interface import INTERFACES
 from arena.paths import PATH_RESULTS
 
 
 @click.command()
-@click.argument('ref')
-@click.argument('aggregator_str')
-@click.argument('objectives_key')
-def main(ref: str, aggregator_str: str, objectives_key: str):
-    A = eval(aggregator_str)
-    column_name = f"{aggregator_str}_{ref}"
+@click.argument("ref")
+@click.argument("representation")
+@click.argument("interface_key")
+@click.argument("objectives_key")
+def main(ref: str, representation: str, interface_key: str, objectives_key: str):
+    interface = INTERFACES[interface_key]
+    fn = interface(representation)
+    column_name = f"{representation}_{ref}"
 
     objectives = OBJECTIVE_LISTS[objectives_key]
 
-    results = {f"{objective}": objective(A) for objective in objectives}
+    results = {f"{objective}": objective(fn) for objective in objectives}
     df = pd.DataFrame(list(results.items()), columns=["Objective", column_name])
 
     PATH_RESULTS.mkdir(parents=True, exist_ok=True)
