6411897f3f677fca32f028d647817e362ddcc277
Valérian Rey <valerian.rey@gmail.com>
2025-05-02 11:05:56 +0200

    Move compute to scripts/

--- compare_upgrad_speed.sh
+++ compare_upgrad_speed.sh
@@ -1,11 +1,11 @@
 echo Installing environment with torchjd at ref main...
 ./setup_env.sh main
 echo Done. Computing objective values...
-uv run python -m compute main "UPGrad()" agg runtime
+uv run python -m scripts.compute main "UPGrad()" agg runtime
 echo Done, result saved in results/
 
 echo Installing environment with torchjd at ref qp_solver...
 ./setup_env.sh qp_solver
 echo Done. Computing objective values...
-uv run python -m compute qp_solver "UPGrad()" agg runtime
+uv run python -m scripts.compute qp_solver "UPGrad()" agg runtime
 echo Done, result saved in results/
