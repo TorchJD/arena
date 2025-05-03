df32bafae1007ad5a0e82937d71d50d3cd5636bf
Valérian Rey <valerian.rey@gmail.com>
2025-05-03 13:28:12 +0200

    Change loop order in configurations.py

--- src/arena/configurations.py
+++ src/arena/configurations.py
@@ -12,10 +12,10 @@ from arena.objectives import (
 OBJECTIVE_LISTS = {
     "runtime": [
         AggregationTime(matrix_sampler=cls(m, m, m-1, dtype), device=device, iterations=1)
+        for cls in [NormalSampler, StrongSampler, StrictlyWeakSampler, NonWeakSampler]
         for dtype in [torch.float32, torch.float64]
         for device in ["cpu", "cuda"]
         for m in [2, 4, 32, 128]
-        for cls in [NormalSampler, StrongSampler, StrictlyWeakSampler, NonWeakSampler]
     ],
     "project_weights": [
         DualProjectionPrimalFeasibilityObjective(m=10, device="cpu", dtype=torch.float32, iterations=10),
