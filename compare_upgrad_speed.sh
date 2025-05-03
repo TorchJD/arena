e1123b8c313a0711538c1a6e36ce7a8d49429c53
Valérian Rey <valerian.rey@gmail.com>
2025-05-03 13:25:07 +0200

    Improve compare_upgrad_speed.sh

--- compare_upgrad_speed.sh
+++ compare_upgrad_speed.sh
@@ -1,11 +1,19 @@
-echo Installing environment with torchjd at ref main...
+echo "Installing environment with torchjd at ref main..."
 ./setup_env.sh main
-echo Done. Computing objective values...
+echo "Done. Computing objective values..."
 uv run python -m scripts.compute main "UPGrad()" agg runtime
-echo Done, result saved in results/
+echo "Done, result saved in results/"
 
-echo Installing environment with torchjd at ref qp_solver...
+echo "Installing environment with torchjd at ref qp_solver..."
 ./setup_env.sh qp_solver
-echo Done. Computing objective values...
+echo "Done. Computing objective values for UPGrad()..."
 uv run python -m scripts.compute qp_solver "UPGrad()" agg runtime
-echo Done, result saved in results/
+echo "Done, result saved in results/"
+
+echo "Done. Computing objective values UPGrad(max_iter=100, eps=0)..."
+uv run python -m scripts.compute qp_solver "UPGrad(max_iter=100, eps=0)" agg runtime
+echo "Done, result saved in results/"
+
+echo "Done. Computing objective values UPGrad(max_iter=1000, eps=0)..."
+uv run python -m scripts.compute qp_solver "UPGrad(max_iter=1000, eps=0)" agg runtime
+echo "Done, result saved in results/"
